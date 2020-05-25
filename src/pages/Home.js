b13635d07fbc7651ea7200743e0d74b8f2947f5f
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-25 21:58:56 +0300

    two

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
+import Services from "../components/Services";
 
 export default function Home() {
     return (
-        <div>
-            home page
-        </div>
+        <>
+            <Hero hero="defaultHero">
+                <Banner title="luxurios rooms" subTitle="deluxe rooms starting at $299">
+                    <Link to='/rooms' className="btn-primary">
+                        our rooms
+                    </Link>
+                </Banner>
+            </Hero>
+            <Services />
+        </>
     );
 }
-
+Hero.defaultProps = {
+    hero: "defaultHero"
+}
 
