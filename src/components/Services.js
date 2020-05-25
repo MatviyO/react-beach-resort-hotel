b1b7ce3f7105f2df965eb034f12ef530d8c9400b
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-26 00:03:58 +0300

    context

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
 import Title from "./Title";
-
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from "react-icons/all";
 
 class Services extends Component {
+    state = {
+        services: [
+            {
+                icon: <FaCocktail />,
+                title: 'free cocktails',
+                info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, rerum!\n'
+            },
+            {
+                icon: <FaHiking />,
+                title: 'Endless Hiking',
+                info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, rerum!\n'
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: 'Free shitle',
+                info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, rerum!\n'
+            },
+            {
+                icon: <FaBeer />,
+                title: 'Strongest Bear',
+                info: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, rerum!\n'
+            }
+
+        ]
+    }
     render() {
         return (
-            <div>
+            <section className="services">
                 <Title title="services" />
-            </div>
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                    })}
+                </div>
+            </section>
         );
     }
 }
