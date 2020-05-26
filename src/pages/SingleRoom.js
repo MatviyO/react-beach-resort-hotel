997c23335fc226c76c0370d809f45914d5291a47
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-27 00:50:26 +0300

    rooms

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import defaultBcg from '../images/room-1.jpeg'
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from 'react-router-dom'
+import { RoomContext} from "../context";
 
-export default function SingleRoom() {
-    return (
-        <div>
-            singleroom page
-        </div>
-    );
+
+class SingleRoom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slug: props.match.params.slug,
+            defaultBcg
+        }
+    }
+    static contextType = RoomContext
+    // componentDidMount() {
+    //
+    // }
+
+    render() {
+        const {getRoom} = this.context
+        const room = getRoom(this.state.slug)
+        if (!room) {
+            return <div className="error">
+                <h3>no such room could be found</h3>
+                <Link to="/rooms" className="btn-primary">back to rooms</Link>
+            </div>
+        }
+        return (
+            <div>
+                singe room {room.name}
+            </div>
+        );
+    }
 }
 
-
+export default SingleRoom;
