6bd18a0b100ee9ba959164ceaefc1706c0d291a4
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-05-26 23:49:18 +0300

    featured

--- src/context.js
+++ src/context.js
@@ -9,7 +9,24 @@ class RoomProvider extends Component {
         featuredRooms: [],
         loading: true
     };
-    
+    componentDidMount() {
+        let rooms = this.formatData(items)
+        let featuredRooms = rooms.filter(room => room.featured === true)
+        this.setState({
+            rooms: rooms, featuredRooms: featuredRooms, sortedRooms: rooms, loading: false
+        })
+
+    }
+    formatData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id
+            let images = item.fields.images.map(image => image.fields.file.url)
+            let room = {...item.fields, images: images, id: id}
+            return room
+        })
+        return tempItems
+    }
+
     render() {
         return (
             <RoomContext.Provider value={{...this.state}}>
